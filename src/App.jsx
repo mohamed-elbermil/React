eebd46d6ae3a9c91eb3ddc39d1cd782c42b9b375
MohamedELBERMIL69780 <mohamed-du-69780@hotmail.fr>
2025-02-03 14:16:06 +0100

    amélioration du code

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,50 @@
 import React from "react";
-import "./App.css";
+import Card from "./card";
+import "../src/App.css";
+import "../src/assets/css/variables.css";
 
-function App() {
+const App = () => {
+  const apartments = [
+    {
+      category: "Entire apartment rental in Collingwood",
+      image: "images/appart-1.png",
+      title: "A Stylish Apt, 5 min walk to Queen Victoria Market",
+      view: "82 reviews",
+      location: "Collingwood VIC",
+      price: 540,
+      details: ["Collingwood VIC ", "1 bed ", "Wi-Fi"],
+    },
+    {
+      view: "82 reviews",
+      details: ["Collingwood VIC ", "1 bed ", "Wi-Fi"],
+      image: "images/appart-2.png",
+      title: "Designer NY style loft",
+      location: "Test VIC",
+      price: 350,
+    },
+    {
+      view: "82 reviews",
+      details: ["Collingwood VIC ", "1 bed ", "Wi-Fi"],
+      image: "images/appart-3.png",
+      title: "5 minute walk from University of Melbourne",
+      location: "Test VIC",
+      price: 350,
+    },
+    {
+      view: "82 reviews",
+      details: ["Collingwood VIC ", "1 bed ", "Wi-Fi"],
+      image: "images/appart-4.png",
+      title: "Magnificent apartment next to public transport",
+      location: "Test VIC",
+      price: 350,
+    },
+  ];
   return (
-    <div className="app-container">
-      <div className="element-next">
-        <div>
-          <h1>223 stays in Melbourne</h1>
-          <p>Book your next stay at one of our properties.</p>
-        </div>
-        <button className="little-btn">Share</button>
-        <button className="long-btn">Save search</button>
-      </div>
-      <section className="all-card">
-        <div className="card">
-          <img src="../public/images/appart-1.png" alt="appartement" />
-          <div className="text-card">
-            <div className="title-icon">
-              <div className="title-left">
-                <span>Entire apartment rental in Collingwood</span>
-                <p>A Stylish Apt, 5 min walk to Queen Victoria Market</p>
-              </div>
-              <div className="heart">
-                <i class="fa-regular fa-heart"></i>
-              </div>
-            </div>
-            <div className="star">
-              <i class="fa-solid fa-star"></i>
-              <i class="fa-solid fa-star"></i>
-              <i class="fa-solid fa-star"></i>
-              <i class="fa-solid fa-star"></i>
-              <i class="fa-solid fa-star"></i>
-              <p>4.9</p>
-              <p className="details">202 reviews</p>
-            </div>
-            <div className="infos-card">
-              <div className="infos-card-left">
-                <div className="info-element details">
-                  <i class="fa-solid fa-location-dot"></i>
-                  <p>Collingwood VIC</p>
-                </div>
-                <div className="info-element details">
-                  <i class="fa-solid fa-house"></i>
-                  <p>1 bed</p>
-                </div>
-                <div className="info-element details">
-                  <i class="fa-solid fa-wifi"></i>
-                  <p>Wifi</p>
-                </div>
-              </div>
-              <div className="info-element details">
-                <p>
-                  <span>$540 </span>AUD total
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+    <div className="all-card">
+      {apartments.map((apt, index) => (
+        <Card key={index} {...apt} />
+      ))}
     </div>
   );
-}
+};
 export default App;
--- src/card.jsx
+++ src/card.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 
-const Card = ({ image, title, location, price }) => {
+const Card = ({ image, title, location, price, view, details }) => {
   return (
     <div className="card">
-      <img src="{{}}" alt="Appartement" className="card-image" />
+      <img src={image} alt="Appartement" className="card-image" />
       <div className="card-content">
-        <h3>{{ title }}</h3>
+        <div className="title-icon">
+          <h3>{title}</h3>
+          <div className="heart">
+            <i class="fa-regular fa-heart"></i>
+          </div>
+        </div>
         <p>{location}</p>
-        <p className="card-price">${price} AUD total</p>
+        <div className="review">
+          <div className="star">
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+          </div>
+          <p>{view}</p>
+        </div>
+        <p className="details">{details}</p>
+        <p className="card-price">
+          <span>${price}</span> AUD total
+        </p>
       </div>
     </div>
   );
